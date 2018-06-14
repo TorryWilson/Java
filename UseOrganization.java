defa9d1361c876773136ac80dd02d477ebc61c89
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-14 19:43:19 +0000

    Update UseOrganization.java

--- UseOrganization.java
+++ UseOrganization.java
@@ -1,15 +1,18 @@
 public class UseOrganization {
 
     public static void main(String args[]) {
-
+// creates an organization
        Organization myOrganization = new Organization();
+// sets info about organization
        myOrganization.setName("LA Cat");
        myOrganization.setProfit(true);
        myOrganization.setRevenue(1000000);
+        
        Organization yourOrganization = new Organization();
        yourOrganization.setName("Blue Shield");
        yourOrganization.setProfit(false);
        yourOrganization.setRevenue(5000);
+        
        myOrganization.display();
        yourOrganization.display();
        System.out.printf("$%3.2f\n", yourOrganization.taxPaying());

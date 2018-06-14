89eb19cc3ea9698925bd972b8b9b8d8df6bc123a
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-14 18:58:09 +0000

    Update Organization.java

--- Organization.java
+++ Organization.java
@@ -1,12 +1,30 @@
 public class Organization {
 
     String name;
-    int revenue;
+    double revenue;
     boolean profit;
-    
+// displays information about the organization
     public void display() {
+        String kind;
+        if (profit == true) {
+            kind = "profit";
+        }
 
-        System.out.print(name + " has" + " " + revenue + " dollars in revenue" );
+        else kind = "non-profit";
+        System.out.println(name + " is a " + kind + " organization that has" + " " + revenue + " dollars in revenue" );
+    }
+
+    // adds a method to class that computes the amount of tax an organization pays.
+    // a profit-making organization pays 10 percent in taxes while a non-profit pays 2.
+    public double taxPaying() {
+        double tax;
+        if (profit == true) {
+            tax = revenue / 10.00;
+        }
+
+            else
+                tax = revenue / 2.00;
+        return tax;
     }
 
 }

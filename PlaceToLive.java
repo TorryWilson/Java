0888140773c9cd66ba23d0742bb8fb175fd1bf2c
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-15 18:50:28 +0000

    Update PlaceToLive.java

--- PlaceToLive.java
+++ PlaceToLive.java
@@ -1,10 +1,15 @@
-public class PlaceToLive {
+// A PlaceToLive has an address, a number of bedrooms, and an area (in square feet or square meters). Write the PlaceToLive class's code.
+// Write code for a separate class named DisplayThePlaces. Your DisplayThePlaces class creates a few PlaceToLive instances by assigning values to their address, numberOfBedrooms, and area fields.
+// The DisplayThePlaces class also reads (from the keyboard) the cost of living in each place. For each place, your code displays the cost per square foot (or square meter) and the cost per bedroom.
 
+public class PlaceToLive {
+// fields that a place to live has
     private String address;
     private int bedrooms;
     private int area;
 
-
+    
+ // getters and setters for fields
     public String getAddress() {
         return address;
     }
@@ -29,6 +34,7 @@ public class PlaceToLive {
         this.area = area;
     }
 
+    // method that calculates cost per square foot and returns it
     public double areaCalc(double costofLiving) {
 
     double perFoot = area / costofLiving;
@@ -36,6 +42,7 @@ public class PlaceToLive {
     return perFoot;
 }
 
+    // method that calculates cost per bed and returns it.
     public double perBed(double costofLiving) {
 
         double bedCost = costofLiving / bedrooms;

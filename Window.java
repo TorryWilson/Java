a42efd85c576914fb24aaba32b4f0ae31f14960a
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-15 17:07:56 +0000

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,11 +1,15 @@
+// dummiesFrame class is needed 
+
 public class Window {
 
     public static void main(String[] args) {
-
+// creates a window instance titled About your organization
         DummiesFrame frame = new DummiesFrame("About your organization");
+// 3 rows for users to enter information about organization
         frame.addRow("Name of Organization");
         frame.addRow("Organization's Revenue");
         frame.addRow("Is it Profit? YES/NO");
+// opens up window
         frame.go();
     }
 
@@ -18,3 +22,5 @@ public class Window {
         return "Your company " + newOrganization.getName() + " pays " + newOrganization.taxPaying() + " in taxes!";
     }
 }
+
+// Task: A window has text fields for an organization's name, annual revenue, and status (profit-making or non-profit-making). When the user clicks a button, the window displays the amount of tax the organization pays.

02110da1239dd10ad9d9ad63a6bada07b9f4b848
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-15 19:56:28 +0000

    Update DisplayThePlacesGUI.java

--- DisplayThePlacesGUI.java
+++ DisplayThePlacesGUI.java
@@ -1,3 +1,5 @@
+// Use your new PlaceToLive class and my DummiesFrame class to create a GUI application. The GUI application takes information about a place to live and displays the place's cost per square foot and the cost per bedroom.
+
 package Live;
 
 public class DisplayThePlacesGUI {

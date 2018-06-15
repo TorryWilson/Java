58a081963207915c43ccbd9b3527189a1e7f7f97
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-15 19:51:04 +0000

    Update PlaceToLive.java

--- PlaceToLive.java
+++ PlaceToLive.java
@@ -1,7 +1,7 @@
 // A PlaceToLive has an address, a number of bedrooms, and an area (in square feet or square meters). Write the PlaceToLive class's code.
 // Write code for a separate class named DisplayThePlaces. Your DisplayThePlaces class creates a few PlaceToLive instances by assigning values to their address, numberOfBedrooms, and area fields.
-// The DisplayThePlaces class also reads (from the keyboard) the cost of living in each place. For each place, your code displays the cost per square foot (or square meter) and the cost per bedroom.
-
+// The DisplayThePlaces class also reads (from the keyboard) the cost of living in each place. For each place, your code displays the cost per square foot (or square meter) and the cost per bedroom
+package Live;
 public class PlaceToLive {
 // fields that a place to live has
     private String address;

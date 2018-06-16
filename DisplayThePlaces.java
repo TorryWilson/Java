c4adeb82780586473a0677a195ee4b7686ffd5a4
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-16 15:45:11 +0000

    Update DisplayThePlaces.java

--- DisplayThePlaces.java
+++ DisplayThePlaces.java
@@ -5,15 +5,22 @@ public class DisplayThePlaces {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        
+        // for loop that creates 3 houses
         for ( int i = 1; i <= 3; i++) {
 
             House newHome = new House();
+            
             System.out.println("Enter mortgage");
             newHome.setMortgage(keyboard.nextDouble());
+            
             System.out.println("Enter number of bedrooms");
             newHome.setBedrooms(keyboard.nextInt());
+            
             System.out.println("Enter area");
             newHome.setArea(keyboard.nextInt());
+            
+            // Should make a display method
             System.out.print("This home is ");
             System.out.printf("$%4.2f\n", newHome.areaCalc(newHome.getArea()));
             System.out.println( " per square foot calculated monthly");
@@ -22,17 +29,23 @@ public class DisplayThePlaces {
             System.out.println( " per bedroom monthly");
              System.out.println();
         }
-
+        
+        
+        // for loop that creates 3 apartments
         for ( int j = 1; j <= 3; j++) {
 
-             Apartment newApartment = new Apartment();
+            Apartment newApartment = new Apartment();
             System.out.println("Enter rent");
+            
             newApartment.setRental(keyboard.nextDouble());
             System.out.println("Enter number of bedrooms");
+            
             newApartment.setBedrooms(keyboard.nextInt());
             System.out.println("Enter area");
+            
             newApartment.setArea(keyboard.nextInt());
             System.out.print("This apartment is ");
+            
             System.out.printf("$%4.2f\n", newApartment.areaCalc(newApartment.getArea()));
             System.out.println( " per square foot calculated monthly");
             System.out.print("This apartment is ");

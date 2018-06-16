3a655926f2e789ec131fc1a67ff03e21411f13c0
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-16 15:35:32 +0000

    Update DisplayThePlaces.java

--- DisplayThePlaces.java
+++ DisplayThePlaces.java
@@ -1,32 +1,64 @@
 package Live;
 
+
+
 import java.util.Scanner;
+
+
 
 public class DisplayThePlaces {
 
 
+
+
+
     public static void main(String[] args) {
+
 // creates a scanner object to write to screen
+
         Scanner keyboard = new Scanner(System.in);
+
         double costOfliving;
+
 // for loop to create three PlacesToLive objects
+
         for ( int i = 1; i <= 3; i++) {
+
             PlaceToLive newPlace = new PlaceToLive();
+
             System.out.println("Enter address");
+
             newPlace.setAddress(keyboard.nextLine());
+
             System.out.println("Enter number of bedrooms");
+
             newPlace.setBedrooms(keyboard.nextInt());
+
             System.out.println("Enter area");
+
             newPlace.setArea(keyboard.nextInt());
+
             System.out.println("Enter cost of living");
+
             costOfliving = keyboard.nextDouble();
+
 // calls to methods to return calculated amound
+
              System.out.println(newPlace.areaCalc(costOfliving));
+
              System.out.println(newPlace.perBed(costOfliving));
+
              System.out.println();
+
+
 
         }
 
+
+
         keyboard.close();
+
     }
+
 }
+

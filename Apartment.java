097986bf9294529f5f0116fe12e019be6057ee21
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-16 10:41:15 -0500

    Add files via upload

---
+++ Apartment.java
@@ -0,0 +1,14 @@
+package Live;
+
+public class Apartment extends PlaceToLive {
+
+    private double rental;
+
+    public double getRental() {
+        return rental;
+    }
+
+    public void setRental(double rental) {
+        this.rental = rental;
+    }
+}
---
+++ House.java
@@ -0,0 +1,25 @@
+package Live;
+
+public class House extends PlaceToLive {
+
+    private double mortgage;
+    private double propertyTax;
+
+    public double getMortgage() {
+        return mortgage;
+    }
+
+    public void setMortgage(double mortgage) {
+        this.mortgage = mortgage;
+    }
+
+    public double getPropertyTax() {
+        return propertyTax;
+    }
+
+    public void setPropertyTax(double propertyTax) {
+        this.propertyTax = propertyTax;
+    }
+}
+
+

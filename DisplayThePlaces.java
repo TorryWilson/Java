71873179577608d04ee3e9484db5a72b8ba9432e
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-16 15:38:20 +0000

    Update DisplayThePlaces.java

--- DisplayThePlaces.java
+++ DisplayThePlaces.java
@@ -1,64 +1,46 @@
 package Live;
-
-
-
 import java.util.Scanner;
-
-
-
 public class DisplayThePlaces {
 
 
-
-
-
     public static void main(String[] args) {
-
-// creates a scanner object to write to screen
-
         Scanner keyboard = new Scanner(System.in);
-
-        double costOfliving;
-
-// for loop to create three PlacesToLive objects
-
         for ( int i = 1; i <= 3; i++) {
 
-            PlaceToLive newPlace = new PlaceToLive();
-
-            System.out.println("Enter address");
-
-            newPlace.setAddress(keyboard.nextLine());
-
+            House newHome = new House();
+            System.out.println("Enter mortgage");
+            newHome.setMortgage(keyboard.nextDouble());
             System.out.println("Enter number of bedrooms");
-
-            newPlace.setBedrooms(keyboard.nextInt());
-
+            newHome.setBedrooms(keyboard.nextInt());
             System.out.println("Enter area");
-
-            newPlace.setArea(keyboard.nextInt());
-
-            System.out.println("Enter cost of living");
-
-            costOfliving = keyboard.nextDouble();
-
-// calls to methods to return calculated amound
-
-             System.out.println(newPlace.areaCalc(costOfliving));
-
-             System.out.println(newPlace.perBed(costOfliving));
-
+            newHome.setArea(keyboard.nextInt());
+            System.out.print("This home is ");
+            System.out.printf("$%4.2f\n", newHome.areaCalc(newHome.getArea()));
+            System.out.println( " per square foot calculated monthly");
+            System.out.print("This home is ");
+            System.out.printf("$%4.2f\n", newHome.perBed(newHome.getMortgage()));
+            System.out.println( " per bedroom monthly");
              System.out.println();
-
-
-
         }
 
+        for ( int j = 1; j <= 3; j++) {
 
+             Apartment newApartment = new Apartment();
+            System.out.println("Enter rent");
+            newApartment.setRental(keyboard.nextDouble());
+            System.out.println("Enter number of bedrooms");
+            newApartment.setBedrooms(keyboard.nextInt());
+            System.out.println("Enter area");
+            newApartment.setArea(keyboard.nextInt());
+            System.out.print("This apartment is ");
+            System.out.printf("$%4.2f\n", newApartment.areaCalc(newApartment.getArea()));
+            System.out.println( " per square foot calculated monthly");
+            System.out.print("This apartment is ");
+            System.out.printf("$%4.2f\n", newApartment.perBed(newApartment.getRental()));
+            System.out.println( " per bedroom monthly");
+            System.out.println();
+        }
 
         keyboard.close();
-
     }
-
 }
-

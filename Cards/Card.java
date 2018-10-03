2c618600b4d91619ebba57d9d379e645efe3ade9
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-10-03 21:09:23 +0000

    Update Card.java

--- Cards/Card.java
+++ Cards/Card.java
@@ -1,18 +1,25 @@
-public class Card {
+package CardDeck;
 
+public class Card {
+    // fields of a Card
     private Suit kind;
     private int number;
 
+    // counter variable that belongs to entire class, reason why its static.
     private static int count;
 
+    // Card constructor
     public Card(Suit kind, int number) {
 
         this.kind = kind;
         this.number = number;
+        
+        // increases count of card
         count++;
 
     }
 
+    // method to convert number to String and also allows the # 11, 12, 13 to be Jack, Queen, King
     public String AlphaConvert() {
 
             String numberString;
@@ -36,7 +43,7 @@ public class Card {
             return numberString + " of " + kind;
 
         }
-
+    // getters and setters
         public Suit getKind() {
             return kind;
         }
@@ -53,6 +60,8 @@ public class Card {
             this.number = number;
         }
 
+    
+    // returns the total count of cards.
         public static int Counter(){
 
         return count;
